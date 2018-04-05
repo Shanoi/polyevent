da7b0b22cdf53d41d71efab9ef69f195b30bc54d
Shanoi <olivboulet@laposte.net>
2018-04-05 19:34:03 +0200

    lol

--- core/src/main/java/fr/unice/polytech/isa/teamk/ResponsibleRegister.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.isa.teamk;
-
-import fr.unice.polytech.isa.teamk.entities.users.User;
-
-public interface ResponsibleRegister {
-
-    boolean registerNewResponsible(User responsible);
-
-}
--- core/src/main/java/fr/unice/polytech/isa/teamk/entities/users/Responsible.java
+++
@@ -1,6 +0,0 @@
-package fr.unice.polytech.isa.teamk.entities.users;
-
-public class Responsible extends User {
-
-
-}
