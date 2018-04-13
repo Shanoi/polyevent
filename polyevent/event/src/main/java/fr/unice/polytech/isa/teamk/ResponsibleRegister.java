6b3588545d0e940afaef8701bce7de76d7d4d79c
Shanoi <olivboulet@laposte.net>
2018-04-14 00:39:35 +0200

    Déplacement de classe

--- polyevent/event/src/main/java/fr/unice/polytech/isa/teamk/ResponsibleRegister.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.isa.teamk;
-
-import fr.unice.polytech.isa.teamk.entities.user.User;
-
-public interface ResponsibleRegister {
-
-    boolean registerNewResponsible(User responsible);
-
-}
